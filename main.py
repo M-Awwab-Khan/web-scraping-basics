f30081541fe44c52519bfa19ca38f02192fc441a
Awwab <mawwabkhank2006@gmail.com>
2024-02-09 23:03:44 +0500

    extracted link text using css selectors

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import requests
 response = requests.get('https://news.ycombinator.com/')
 page = response.text
 
+soup = BeautifulSoup(page, 'html.parser')
+titles = [item.text for item in soup.select('.titleline a:not(.sitebit.comhead a)')]
+
