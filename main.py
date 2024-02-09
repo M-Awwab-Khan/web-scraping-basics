6fb8056c77c57ff232e94506d82aa2a53a0316aa
Awwab <mawwabkhank2006@gmail.com>
2024-02-09 23:03:55 +0500

    printed titles in formatted way

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ page = response.text
 soup = BeautifulSoup(page, 'html.parser')
 titles = [item.text for item in soup.select('.titleline a:not(.sitebit.comhead a)')]
 
+for i in range(1, len(titles)+1):
+    print(f"{i}. {titles[i]}")
