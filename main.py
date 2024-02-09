fe21a8d3d01f12adbf3b36afc5f9d72d4f4c4954
Awwab <mawwabkhank2006@gmail.com>
2024-02-09 23:09:35 +0500

    extracted upvotes

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ page = response.text
 
 soup = BeautifulSoup(page, 'html.parser')
 titles = [item.text for item in soup.select('.titleline a:not(.sitebit.comhead a)')]
+upvotes = [item.text for item in soup.select('.subline .score')]
 
-for i in range(1, len(titles)+1):
-    print(f"{i}. {titles[i]}")
+
+for i in range(0, len(titles)+1):
+    print(f"{i+1}. {titles[i]} : {upvotes[i]}")
