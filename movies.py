80dd0a0ebf47969586b4eedea329319ace83ee35
Awwab <mawwabkhank2006@gmail.com>
2024-02-10 10:50:56 +0500

    moved url to constant

--- movies.py
+++ movies.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests 
-
-response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
+URL = 'https://www.empireonline.com/movies/features/best-movies-2/'
+response = requests.get(URL)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 titles = [item.text for item in soup.select('.listicleItem_listicle-item__title__BfenH')]
